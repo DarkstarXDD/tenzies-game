6bf392738a943da915191ed1b21ae853461f98eb
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-07-22 23:26:54 +0530

    Initialize project with Vite, assets & styles

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <main className="main">
+      <h1 className="title">Tenzies</h1>
+      <p className="text">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+    </main>
+  )
+}
