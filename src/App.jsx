95631a26e36fe104d0b1b7f5ff28bafe65a96958
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-07-23 23:43:58 +0530

    Add dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import React from "react"
+import Die from "./components/Die"
+
 export default function App() {
+  function getRandomNumbers() {
+    let randomNumbers = []
+    let randomNumber = 0
+
+    for (let i = 0; i < 10; i++) {
+      randomNumber = Math.ceil(Math.random() * 6)
+      randomNumbers.push(randomNumber)
+    }
+    return randomNumbers
+  }
+
+  const [dieNumbers, setDieNumbers] = React.useState(getRandomNumbers)
+
+  const dice = dieNumbers.map((dieNumber) => <Die value={dieNumber} />)
+
+  function handleRollClick() {
+    setDieNumbers(getRandomNumbers())
+  }
+
   return (
     <main className="main">
       <h1 className="title">Tenzies</h1>
@@ -6,6 +28,11 @@ export default function App() {
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
+
+      <div className="die-group">{dice}</div>
+      <button onClick={handleRollClick} className="btn__roll">
+        Roll
+      </button>
     </main>
   )
 }
